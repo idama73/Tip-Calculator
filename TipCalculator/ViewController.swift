a849bfb732368db6a88ddf5572b102fe474aa039
Idama Okumagba <Idamarire@gmail.com>
2019-09-04 14:20:17 -0700

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by idamarire okumagba on 9/4/19.
+//  Copyright © 2019 idamarire okumagba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
